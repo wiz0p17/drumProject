bb2de991a462f4491aad7488489fdb348caec0ff
wiz0p17 <hyperwiz0p@gmail.com>
2025-01-22 11:53:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+
+for(var i=0;i<7;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function (){
+        var keyPressed= this.innerHTML;
+        evenCheck(keyPressed);
+        buttonAnimation(keyPressed);
+    });
+}
+
+
+function evenCheck(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio('/sounds/crash.mp3');
+            audio.play();
+            break;
+        
+        case "a":
+            var audio = new Audio('/sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('/sounds/snare.mp3');
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio('/sounds/tom-1.mp3');
+            audio.play();
+            break;
+            
+        case "j":
+            var audio = new Audio('/sounds/tom-2.mp3');
+            audio.play();
+            break;
+     
+        case "k":
+            var audio = new Audio('/sounds/tom-3.mp3');
+            audio.play();
+            break;
+            
+        case "l":
+            var audio = new Audio('/sounds/tom-4.mp3');
+            audio.play();
+            break;
+    }
+}
+
+document.addEventListener("keydown",function (){
+    evenCheck(event.key);
+    buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey){
+    var activeButton=document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function (){
+        activeButton.classList.remove("pressed")
+    },100);
+}
