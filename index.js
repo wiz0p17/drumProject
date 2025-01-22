6bb5ee8461dd282cd0930611511306d55895a921
wiz0p17 <hyperwiz0p@gmail.com>
2025-01-22 12:02:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,38 +11,39 @@ for(var i=0;i<7;i++){
 function evenCheck(key){
     switch (key) {
         case "w":
-            var audio = new Audio('sounds/crash.mp3');
-            audio.play();
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
             break;
         
         case "a":
-            var audio = new Audio('sounds/kick-bass.mp3');
-            audio.play();
+            var kickBass = new Audio('sounds/kick-bass.mp3');
+            kickBass.play();
             break;
         case "s":
-            var audio = new Audio('sounds/snare.mp3');
-            audio.play();
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
             break;
 
         case "d":
-            var audio = new Audio('sounds/tom-1.mp3');
-            audio.play();
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
             break;
             
         case "j":
-            var audio = new Audio('sounds/tom-2.mp3');
-            audio.play();
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
             break;
      
         case "k":
-            var audio = new Audio('sounds/tom-3.mp3');
-            audio.play();
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
             break;
             
         case "l":
-            var audio = new Audio('sounds/tom-4.mp3');
-            audio.play();
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
             break;
+        default: console.log(key);
     }
 }
 
